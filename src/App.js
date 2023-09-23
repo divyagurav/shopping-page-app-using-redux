fac859b17a2a36110b0d87aab66aa5245f0f49b3
divyagurav <132669878+divyagurav@users.noreply.github.com>
2023-09-23 21:31:38 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,54 +1,47 @@
-import {Fragment,useEffect} from 'react';
+import { Fragment, useEffect } from "react";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
-import Notification from './components/UI/Notification';
+import Notification from "./components/UI/Notification";
 
-import { useSelector,useDispatch } from "react-redux";
-import {sendCartData,fetchCartData} from './store/cart-actions';
+import { useSelector, useDispatch } from "react-redux";
+import { sendCartData, fetchCartData } from "./store/cart-actions";
 
-
-
-
-let isInitial=true;
+let isInitial = true;
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
-  const cart=useSelector((state)=>state.cart);
-  const dispatch=useDispatch();
-  
-  const notification=useSelector(state=>state.ui.notification)
+  const cart = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
 
-  useEffect(()=>{
-dispatch(fetchCartData())
-  },[dispatch])
+  const notification = useSelector((state) => state.ui.notification);
 
- useEffect(()=>{
-   
-    
-if(isInitial){
-  isInitial=false;
-  return;
-}
+  useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
 
-dispatch(sendCartData(cart))
+  useEffect(() => {
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
 
- 
-
-
- },[cart,dispatch]);
-
-
-
-
+    dispatch(sendCartData(cart));
+  }, [cart, dispatch]);
 
   return (
     <Fragment>
-   {notification&&<Notification status={notification.status} title={notification.title} message={notification.message}/>}
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
-     <Fragment/>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
